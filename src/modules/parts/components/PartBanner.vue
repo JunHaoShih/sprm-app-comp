2a64cf759f57cefad874e3d38201c6913f53655a
JunHaoShih <victor5517@gmail.com>
2023-06-15 15:05:02 +0800

    feat: add icon to part banner

--- src/modules/parts/components/PartBanner.vue
+++ src/modules/parts/components/PartBanner.vue
@@ -5,6 +5,7 @@
       style="border-radius: 10px"
     >
       <div class="text-h6 row">
+        <q-icon name="settings" size="24px" class="q-mt-xs q-mr-sm"/>
         <div>{{ part.number }}</div>
         <div
           v-if="part.checkout"
