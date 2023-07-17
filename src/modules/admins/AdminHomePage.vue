a66cc7b3b9cf5246e53e26ef7d693744a26fc87a
JunHaoShih <victor5517@gmail.com>
2023-07-17 13:36:03 +0800

    fix: make admin page center properly

--- src/modules/admins/AdminHomePage.vue
+++ src/modules/admins/AdminHomePage.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>Admin home page</div>
+  <div class="row justify-center items-center center">
+    <div class="self-center text-h1">( ｀・ω・´)</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 </script>
+
+<style lang="sass">
+.center
+  height: calc( 100vh - 50px )
+</style>
